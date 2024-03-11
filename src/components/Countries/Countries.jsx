e1c7a4be9f176a9f79595680c8ee92ff08cab84a
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-11 10:39:17 +0600

    last commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,6 +5,13 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [countryDetails, setCountryDetails] = useState([]);
+    const handleButton = (country) => {
+        console.log(country);
+        const newCart = [...countryDetails, country];
+        setCountryDetails(newCart);
+    }
+    console.log(countryDetails);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
@@ -13,12 +20,19 @@ const Countries = () => {
 
     return (
         <div>
+            <div>
+                {countryDetails.map(country => <li
+                    key={country.cca2}
+                
+                >{country.name.common}</li>)}
+            </div>
             <h3>Countries: {countries.length}</h3>
             <div className="countries">
                 {
                     countries.map(country => <Country
                         key={country.cca2}
                         country={country}
+                        handleButton={handleButton}
                     ></Country>)
                 }
             </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, handleButton }) => {
     const { capital,population,area,cca3, flags: { png }} = country
     // console.log(country);
     const [visited, setVisited] = useState(false);
@@ -16,8 +16,10 @@ const Country = ({ country }) => {
             <h5>Population: { population}</h5>
             <h5>Area: {area}</h5>
             <p>Code: <small>{cca3}</small></p>
-            <button  onClick={handleVisited} className='button'>{visited?'Visited' :'Going'}</button>
-            {visited ? 'I have visited this country':'I want to visit'}
+            <button onClick={handleVisited} className='button'>
+                {visited ? 'Visited' : 'Going'}</button>
+            {visited ? 'I have visited this country' : 'I want to visit'} <br /><br />
+            <button onClick={()=>handleButton(country)} className='button'>Details</button>
         </div>
     );
 };
