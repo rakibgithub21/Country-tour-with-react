32932b942063669c05933730c9b93a54d659da7e
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-11 06:30:29 +0600

    details country details

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,14 @@
 
 import './Country.css'
-const Country = ({ country }) => {
-    const {capital,flags:{png}} = country
+const Country = ({ country, }) => {
+    const { capital,population,area, flags: { png }} = country
     console.log(country);
     return (
         <div className='country'>
             <h3>Name: {country.name.common}</h3>
             <h4>Capital: {capital}</h4>
+            <h5>Population: { population}</h5>
+            <h5>Area: { area}</h5>
             <img src={png} alt="" />
         </div>
     );
